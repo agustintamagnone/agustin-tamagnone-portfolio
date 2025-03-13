a65b65235f8763ece5f949e7c19f2b28b58ce749
Agustin Tamagnone <agustintamagnone1@hotmail.com>
2025-03-13 00:12:27 -0300

    Versioning untracked files

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  transpilePackages: ["geist"],
-};
-
-export default nextConfig;
