6402498072be1abaa271069ee90429385d663065
Agustin Tamagnone <agustintamagnone1@hotmail.com>
2025-03-13 18:02:37 -0300

    New files and config for dreamhost

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // DreamHost does not support Next.js Image Optimization
+  },
   reactStrictMode: true,
   transpilePackages: ["geist"],
-  output: "export", // ✅ Ensure Next.js exports static files
-  distDir: "out",  // ✅ Netlify uses the `out` folder
-  images: {
-    unoptimized: true, // ✅ Ensure images work in static export mode
-  },
 };
 
 export default nextConfig;
